2ec8c192cf2c2f8c4ab9fbb2891af1435d4800b5
daviedev <daviedev@07638116-a63d-0410-b52e-83cba363e8d1>
2011-02-27 14:08:02 +0000

    Replaced ioctl's with more portable tc?etattr()

--- src/secure_input.c
+++ src/secure_input.c
@@ -33,7 +33,7 @@ secure_input(char *buf, size_t buf_size)
 	DWORD mode;
 	HANDLE ih;
 #else
-	struct termio ttymode;
+	struct termios ttymode;
 #endif
 	int pos;
 	int ch;
@@ -51,9 +51,9 @@ secure_input(char *buf, size_t buf_size)
 	}
 	SetConsoleMode(ih, mode & ~(ENABLE_ECHO_INPUT ));
 #else
-	ioctl(STDIN_FILENO, TCGETA, &ttymode);
+	tcgetattr(STDIN_FILENO, &ttymode);
 	ttymode.c_lflag &= ~( ECHO | ECHOE | ECHONL );
-	ioctl(STDIN_FILENO, TCSETAF, &ttymode);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &ttymode);
 #endif
 
 	pos=0;
@@ -75,7 +75,7 @@ secure_input(char *buf, size_t buf_size)
 	fputc('\n', stdout);
 #else
 	ttymode.c_lflag |= ECHO | ECHOE | ECHONL;
-	ioctl(STDIN_FILENO, TCSETAF, &ttymode);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &ttymode);
 	fputc('\n', stdout);
 #endif
 }
